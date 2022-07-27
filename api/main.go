5d095d4f189bdc0e7e05a9bca6386bc2aab3db0a
Mani Movassagh <mani.mgh@gmail.com>
2022-07-27 19:45:54 +0200

    add fiber

--- api/main.go
+++ api/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+	"github.com/manimovassagh/shortenURLFiberRedis/routes"
+)
+
+func setupRoutes(app *fiber.app) {
+	app.Get("/:url", routes.ResolveURL)
+	app.Post("/api/v1", routes.ShortenURL)
+
+}
 
 func main() {
-	fmt.Println("Start Project")
+	godotenv.Load()
+	app := fiber.New()
+	app.Listen("3000")
 }
--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -1,4 +1,16 @@
 package routes
 
-type request struct{}
-type response struct{}
+import "time"
+
+type request struct {
+	URL         string        `json:"url"`
+	CustomShort string        `json:"short"`
+	Expiry      time.Duration `json:"expiry"`
+}
+type response struct {
+	URL             string        `json:"url"`
+	CustomShort     string        `json:"short"`
+	Expiry          time.Duration `json:"expiry"`
+	XRateRemaining  int           `json:"rate_limit"`
+	XRateLimitReset time.Duration `json:"rate_limit_reset"`
+}

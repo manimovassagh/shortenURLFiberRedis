bf86b8f689ca8b3a0d9828f2d5b5f8ce35e5ec38
Mani Movassagh <mani.mgh@gmail.com>
2022-07-27 19:55:59 +0200

    define ports

--- api/main.go
+++ api/main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/joho/godotenv"
 	"github.com/manimovassagh/shortenURLFiberRedis/routes"
 )
@@ -13,7 +18,12 @@ func setupRoutes(app *fiber.app) {
 }
 
 func main() {
-	godotenv.Load()
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println(err)
+	}
 	app := fiber.New()
-	app.Listen("3000")
+	app.Use(logger.New())
+	setupRoutes(app)
+	log.Fatal(app.Listen(os.Getenv("APP_PORT")))
 }
